1e3ed0e38b74a026d5ee60423fe1490bccf70b0a
RobertoJimenez25 <robertojimenez75@aragon.unam.mx>
2025-05-08 20:29:12 -0600

    rutas

--- 05052025/rutas/routes/rutas.js
+++ 05052025/rutas/routes/rutas.js
@@ -4,10 +4,29 @@ const path = require('path');
 
 //defincion de las rutas y menejadores
 
+//ruta 1
 router.get("/",(req,res)=>{
-    
-    res.sendFile(path.join(__dirname +"/../indextwo.html"));
+     res.sendFile(path.join(__dirname +"/../index.html"));
 });
 
 
+// //ruta 2
+// router.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname +"/../index2.html"));
+// });
+
+
+// //ruta 3
+//  router.get("/",(req,res)=>{
+//      res.sendFile(path.join(__dirname +"/../index3.html"));
+// });
+
+
+// //ruta 4
+// router.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname +"/../index4.html"));
+// });
+
 module.exports=router;
+
+
