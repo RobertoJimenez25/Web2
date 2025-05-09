281a4f866c724b3ffc603bdb63a42e51b1a1abf3
RobertoJimenez25 <robertojimenez75@aragon.unam.mx>
2025-05-08 20:10:10 -0600

    tarea codigo

--- 05052025/app.js
+++ 05052025/app.js
@@ -1,19 +1,21 @@
-//importacion de express
-const express = require('express');
+const express=require('express');
+const path=require('path');
 
-
-//instancia de express
-const app = express();
+const app=express();
 
 //puerto
-const port = 3000;
+const port=5076;
 
-app.get('/', (req, res) => {
-    res.send('Hola mundo!')
-    res.sendFile("c:/Users/Andres/Documents/Escuela/Programacion-web2/05-05-25/index.html")
+
+app.get("/",(req,res)=>{
     
+    //res.send("HolaMundo")
+    
+    //res.sendFile("C:/Users/salva/Desktop/WEb2/05052025/index.html");
+    
+    res.sendFile(path.join(__dirname+"/index.html"));
 });
-
-app.listen(port, () => {
-    console.log('http://127.0.0.1:${3000}');
+//salida del servidor
+app.listen(port,()=>{
+    console.log(`http://127.0.0.1:${port}`);
 });
